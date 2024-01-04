6565d8798f044307f04eeb0baf93c98f71746b41
= <=>
2024-01-04 22:11:44 +0700

    js

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,18 +1,1 @@
-
-function getClassByRate(vote){
-    if (vote>=8){
-        return "green";
-    } else if (vote>=5){
-        return "orange";
-    } else {
-        return "red";
-    }
-}
-form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    const searchTerm = search.value;
-    if (searchTerm){
-        getMovies(SEARCHAPI + searchTerm);
-        search.value = "";
-    }
-});
+console.log('Hello World');
